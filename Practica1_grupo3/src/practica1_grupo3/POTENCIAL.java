e1a0270f2bf959df2bbfaacb3aa8620849669557
jjenano <javierf.1995@gmail.com>
2018-08-21 22:06:30 -0600

    POTENCIAL
    201314285

---
+++ Practica1_grupo3/src/practica1_grupo3/POTENCIAL.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1_grupo3;
+
+/**
+ *
+ * @author Jorge
+ */
+public class POTENCIAL 
+{
+    
+    public POTENCIAL (int NumeroBase)
+    {
+        int resultado = Pot(NumeroBase);
+        System.out.println(NumeroBase + " elevado a la 2 es igual a " + resultado);
+    }
+    
+    int Pot (int NumeroBase)
+    {
+        int resultado = (int) Math.pow(NumeroBase, 2);
+        return resultado;
+    }
+}
