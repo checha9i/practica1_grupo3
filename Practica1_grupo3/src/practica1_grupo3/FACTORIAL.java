c7fd36d29af6f28879ef2fc26feebfb65e03e4e9
Javier Hevia <javierhc22@gmail.com>
2018-08-21 22:02:09 -0600

    FACTORIAL
    201313898

---
+++ Practica1_grupo3/src/practica1_grupo3/FACTORIAL.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1_grupo3;
+
+/**
+ *
+ * @author Javier
+ */
+public class FACTORIAL {
+      public double factorial (int numero){
+        double factorial = 1;     
+        
+          while ( numero!=0) {
+            factorial=factorial*numero;
+            numero--;
+          }
+          System.out.println(factorial);
+        
+    return  factorial;
+    }
+}
